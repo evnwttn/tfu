42362e3035a7c6c8889099307c92f9875b952c27
evnwttn <evnwttn@gmail.com>
2021-08-07 13:14:54 -0230

    yes/no randomizer

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const opn = document.querySelector("#opn");
 const slct = document.querySelector("#slct");
-    const sYn = document.querySelector("sYn");
-    const sInpt = document.querySelector("sInpt");
-    const sOtpt = document.querySelector("sOtpt");
+    const slctYn = document.querySelector("#slctYn");
+    const slctIn = document.querySelector("#slctIn");
+    const slctOut = document.querySelector("#slctOut");
+const yn = document.querySelector("#yn");
 
     opn.addEventListener("click", (event) => {
         alert("yes");
@@ -10,4 +11,32 @@ const slct = document.querySelector("#slct");
         slct.classList.remove("invisible");
     })
 
+    slctYn.addEventListener("click", (event) => {
+        alert("yesyes");
+        slct.classList.add("invisible");
+        yn.classList.remove("invisible");
+        runYn();
+    })
     
+    slctIn.addEventListener("click", (event) => {
+        alert("yesyesyes");
+        slct.classList.add("invisible");
+    })
+
+    slctOut.addEventListener("click", (event) => {
+        alert("yesyesyesyes");
+        slct.classList.add("invisible");
+    })
+
+
+// FUNCTIONS
+
+function runYn() { // YES OR NO
+let yni = ["yes.",
+            "no."];
+let ynj = yni[Math.floor(Math.random() * yni.length)];
+    document.getElementById("yn").innerHTML = `${ynj}`;
+    yn.addEventListener("click", (event) => {
+        runYn();
+    })
+}
